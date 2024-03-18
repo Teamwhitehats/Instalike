4e8214c15300c9ce95614519e378dc21b1f3bd02
Teamwhitehats <148607261+Teamwhitehats@users.noreply.github.com>
2024-03-18 18:08:20 +0530

    Update and rename Insta to Insta.py

--- Insta.py
+++ Insta.py
@@ -13,13 +13,13 @@ def MyApp():
         req = requests.post("https://api.likesjet.com/freeboost/7", json={"link": link,"instagram_username": user,"email": email}).text
         if 'Success!' in req:
             c += 1
-            put_html(f'<h3>✅ I Can Send Some Likes.</h3><i> BY: SANCHIT</i>')
+            put_html(f'<h3>✅ I Can Send Some Likes.</h3><i> BY: Team Whitehat</i>')
             time.sleep(0.1)
         elif 'once' in req:
-         put_html(f'<h3>❌ You Can Send Likes Already.</h3><i> BY: SANCHIT</i>')
+         put_html(f'<h3>❌ You Can Send Likes Already.</h3><i> BY: Team whitehat</i>')
         else:
             f += 1
-            put_html(f'<h3>❌ Error While Sending Likes.</h3><i> BY: SANCHIT</i>')
+            put_html(f'<h3>❌ Error While Sending Likes.</h3><i> BY: Team whitehat</i>')
             time.sleep(0.1)
 
 if __name__ == '__main__':
